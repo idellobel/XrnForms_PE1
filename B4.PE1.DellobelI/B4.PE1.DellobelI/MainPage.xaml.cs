c58fd77dbd3caced584eedc3f8bbd3539f9a136c
idellobel <ivan.dellobel@gmail.com>
2017-10-09 17:18:28 +0200

    Werking Gallery Page

--- B4.PE1.DellobelI/B4.PE1.DellobelI/MainPage.xaml.cs
+++ B4.PE1.DellobelI/B4.PE1.DellobelI/MainPage.xaml.cs
@@ -15,12 +15,18 @@ namespace B4.PE1.DellobelI
 
             var msgService = DependencyService.Get<IMessageService>();
             lblWelcome.Text = "Opdracht B4.PE1.DellobelI op " + msgService.GetWelcomeMessage();
-        }
+                 }
+
 
         private async void btnPictureGallery_Clicked(object sender, EventArgs e)
         {
-            //navigate to Picture gallery
-            await Navigation.PushAsync(new PictureGallery());
+            await Navigation.PushModalAsync(new NavigationPage(new PictureGallery()));
+           
+        }
+
+        private void btnWiki_Clicked(object sender, EventArgs e)
+        {
+            //await Navigation.PushModalAsync(new NavigationPage(new WikiPage()));
         }
     }
 }
--- B4.PE1.DellobelI/B4.PE1.DellobelI/PictureGallery.xaml.cs
+++ B4.PE1.DellobelI/B4.PE1.DellobelI/PictureGallery.xaml.cs
@@ -19,13 +19,71 @@ namespace B4.PE1.DellobelI
 
         private async void btnGoBack_Clicked(object sender, EventArgs e)
         {
-            await Navigation.PopAsync(true);
+            await Navigation.PopModalAsync();
 
         }
 
-        private void Button_Clicked(object sender, EventArgs e)
+        private async void TapGestureRecognizer_Tapped(object sender, EventArgs e)
         {
+            GestureState gestureState;
+            Image imageSender = (Image) sender;
+           
+            await Navigation.PushAsync(new PhotoPage(imageSender));
+            gestureState = GestureState.Ended;
 
         }
+
+
     }
+    public class PhotoPage : ContentPage
+    {
+
+        public PhotoPage(Image gallery)
+        {
+            var GetImage = gallery.Source.ToString();
+
+            Image Large = new Image();
+            Large.Source = GetImage.Substring(6);
+
+            Button btnBackGallery = new Button
+            {
+                Text = "Back to Picture Gallery",
+                Style = (Style)Application.Current.Resources["CancelButtonStyle"],
+                Margin = new Thickness(5)
+            };
+
+            btnBackGallery.Clicked += BtnClose_Clicked;
+
+            Style = (Style)Application.Current.Resources["ContentPageStyle"];
+            Title = "Foto Large";
+            Content = new ScrollView()
+            {
+                HorizontalOptions = LayoutOptions.CenterAndExpand,
+
+                Content = new StackLayout()
+                {
+
+                   
+                    Children = {
+                                Large,
+                                btnBackGallery
+                                },
+
+                }
+
+            };
+        }
+
+        
+        private async void BtnClose_Clicked(object sender, EventArgs e)
+        {
+           await Navigation.PopAsync();
+        }
+
+
+
+    }
+   
 }
+
+    
